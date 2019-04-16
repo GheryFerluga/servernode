a98f7bdf3d9ec06488a67ece01c9c5e3bc2f9581
GheryFerluga <gherardo.ferluga@gmail.com>
2019-04-16 10:24:47 +0200

    primo server node.js

---
+++ index.js
@@ -0,0 +1,15 @@
+//semplice web server
+//modulo http
+const http = require ('http');
+
+//comportamento web server con call back
+const server = http.createServer(function (req,res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('Ciao World!');
+    res.end();
+})
+//in ascolto
+//server.listen(porta,url);
+
+server.listen(8080, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:8080/');
