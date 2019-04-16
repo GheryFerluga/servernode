70b868643ede0ea2e70b58137e05217aa451cbd7
GheryFerluga <gherardo.ferluga@gmail.com>
2019-04-16 11:35:59 +0200

    add html code

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const http = require ('http');
 //comportamento web server con call back
 const server = http.createServer(function (req,res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('Ciao World!');
+    res.write('<h1>Ciao World!</h1>');
     res.end();
 })
 //in ascolto
