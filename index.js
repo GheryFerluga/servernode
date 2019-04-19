000914209e8997e71935050ce5eb7c7fa6f30b52
GheryFerluga <gherardo.ferluga@gmail.com>
2019-04-19 12:29:35 +0200

    add API

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const express = require('express')
 const app = express()
-//let port = process.env.PORT || 3000;
-let port = process.argv[2] || 3000;
+const fs = require('fs')
+let port= process.argv[2] || 8080
 
-app.use(express.static(__dirname + '/public'))
 
-//app.get('/', function (req, res)  {
-  //res.send('Hello World')
-//})
+app.get('/books',function(req,res){
+  const filename=process.argv[3]
+  fs.readFile(filename,function(e,data){
+    if(e)return res.sendStatus(500)
+    try {
+      books = JSON.parse(data)
+    }catch (e){
+      res.sendStatus(500)
+    }
+    res.json(books)
+  })
+})
+app.get('/menu',function(req,res){
+  const filename=process.argv[3]
+  fs.readFile(filename,function(e,data){
+    if(e)return res.sendStatus(500)
+    try {
+      menu = JSON.parse(data)
+    }catch (e){
+      res.sendStatus(500)
+    }
+    res.json(menu)
+  })
+})
 
- 
+
 app.listen(port)
-console.log(`Server running at http://127.0.0.1:${port}`)
