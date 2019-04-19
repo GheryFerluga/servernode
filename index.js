6cbe282f178a792bd861d0063bbe7812871058d3
GheryFerluga <gherardo.ferluga@gmail.com>
2019-04-19 12:48:39 +0200

    add userse.json

--- index.js
+++ index.js
@@ -40,6 +40,17 @@ app.get('/settings',function(req,res){
     res.json(settings)
   })
 })
-
+app.get('/users',function(req,res){
+  const filename='users.json'
+  fs.readFile(filename,function(e,data){
+    if(e)return res.sendStatus(500)
+    try {
+      users = JSON.parse(data)
+    }catch (e){
+      res.sendStatus(500)
+    }
+    res.json(users)
+  })
+})
 
 app.listen(port)
